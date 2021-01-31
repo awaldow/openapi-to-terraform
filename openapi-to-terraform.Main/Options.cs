dc40613be6c087fbc09797f21ad272e6b48e2722
Addison Waldow <addison.waldow@phoenix.gov>
2021-01-30 18:32:24 -0700

    add option for output directory

--- openapi-to-terraform.Main/Options.cs
+++ openapi-to-terraform.Main/Options.cs
@@ -6,5 +6,8 @@ namespace openapi_to_terraform.Main
     {
         [Option('f', "input-file", Required = true, HelpText = "Absolute or relative path to swagger json input file")]
         public string InputFile { get; set; }
+
+        [Option('o', "output-dir", Required = true, HelpText = "Absolute or relative path of output directory for terraform files")]
+        public string OutputDirectory { get; set; }
     }
 }
