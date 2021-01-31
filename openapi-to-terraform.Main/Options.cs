20abe4a9a5972b744996df1c01aa018f3dd1074d
Addison Waldow <addison.waldow@phoenix.gov>
2021-01-31 15:20:09 -0700

    add some sample files for testing template, revision and terraform variables functionality

--- openapi-to-terraform.Main/Options.cs
+++ openapi-to-terraform.Main/Options.cs
@@ -7,16 +7,19 @@ namespace openapi_to_terraform.Main
         [Option('f', "input-file", Required = true, HelpText = "Absolute or relative path to swagger json input file")]
         public string InputFile { get; set; }
 
-        [Option('o', "output-dir", Required = true, HelpText = "Absolute or relative path of output directory for terraform files")]
+        [Option('o', "output-dir", Required = true,  HelpText = "Absolute or relative path of output directory for terraform files")]
         public string OutputDirectory { get; set; }
 
         [Option('r', "revision-map-file", Required = false, HelpText = "Absolute or relative path to revision mapping json file")]
         public string RevisionFile { get; set; }
 
-        [Option("api-template-file", Required = false, HelpText = "Absolute or relative path to desired template of APIs terraform file")]
+        [Option('t', "terraform-vars-file", SetName = "terraform-vars", HelpText = "Absolute or relative path to file mapping terraform variables to output values")]
+        public string TerraformVariablesFile { get; set; }
+        
+        [Option("api-template-file", SetName = "template-provided", HelpText = "Absolute or relative path to desired template of APIs terraform file")]
         public string ApiTemplateFile { get; set; }
 
-        [Option("operation-template-file", Required = false, HelpText = "Absolute or relative path to desired template of Operations terraform file")]
+        [Option("operation-template-file", SetName = "template-provided", HelpText = "Absolute or relative path to desired template of Operations terraform file")]
         public string OperationTemplateFile { get; set; }
     }
 }
