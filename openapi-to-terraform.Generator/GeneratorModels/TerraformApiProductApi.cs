761a9ebdb098da9d72d32b9d8bc992a267ddf6ab
Addison Waldow <addison.waldow@phoenix.gov>
2021-02-01 15:16:24 -0700

    add left curly

--- openapi-to-terraform.Generator/GeneratorModels/TerraformApiProductApi.cs
+++ openapi-to-terraform.Generator/GeneratorModels/TerraformApiProductApi.cs
@@ -9,7 +9,7 @@ namespace openapi_to_terraform.Generator.GeneratorModels
         {
             StringBuilder sb = new StringBuilder();
             string resourceName = document.Info.Title.ToLower().Replace(" ", "");
-            sb.AppendLine($"resource \"azurerm_api_management_product_api\" \"{resourceName}productapi\"");
+            sb.AppendLine($"resource \"azurerm_api_management_product_api\" \"{resourceName}productapi\" {{");
             sb.AppendLine($"\tapi_name\t=\tazurerm_api_management_api.{resourceName}.name");
             sb.AppendLine($"\tproduct_id\t=\t{{api_management_product_id}}");
             sb.AppendLine($"\tapi_management_name\t=\t{{api_management_service_name}}");
