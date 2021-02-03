41b8922aa4c8f45fa6d1613b5b26e5c49dd3290e
Addison Waldow <addison.waldow@phoenix.gov>
2021-02-03 09:07:57 -0700

    handle version set id

--- openapi-to-terraform.Generator/VariablesAppliers/ApiVariablesApplier.cs
+++ openapi-to-terraform.Generator/VariablesAppliers/ApiVariablesApplier.cs
@@ -18,12 +18,14 @@ namespace openapi_to_terraform.Generator.VariablesAppliers
             var apiPath = terraformVariables.SelectToken("api_path").Value<string>();
             var apiBackendUrl = terraformVariables.SelectToken("api_backend_url").Value<string>();
             var apiManagementProductId = terraformVariables.SelectToken("api_management_product_id").Value<string>();
-            
+            var apiVersionSetId = terraformVariables.SelectToken("api_managment_version_set_id").Value<string>();
+
             var outputVariablesApplied = generatedOutput.Replace("{api_management_service_name}", serviceName);
             outputVariablesApplied = outputVariablesApplied.Replace("{api_management_resource_group_name}", resourceGroupName);
             outputVariablesApplied = outputVariablesApplied.Replace("{api_path}", apiPath);
             outputVariablesApplied = outputVariablesApplied.Replace("{api_backend_url}", apiBackendUrl);
             outputVariablesApplied = outputVariablesApplied.Replace("{api_management_product_id}", apiManagementProductId);
+            outputVariablesApplied = outputVariablesApplied.Replace("{api_managment_version_set_id}", apiVersionSetId);
             return outputVariablesApplied;
         }
     }
