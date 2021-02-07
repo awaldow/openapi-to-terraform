66c4bce7ee4d213fc86571f91870dbf2d16ebb0d
Addison Waldow <addison.waldow@phoenix.gov>
2021-02-07 11:52:42 -0700

    create separate project with minimal dependencies for revision generation from cli

---
+++ openapi-to-terraform.RevisionCli/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace openapi_to_terraform.RevisionCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputAssemblyPath = args[0];
+            var openApiPath = args[1];
+            var outputPath = args[2];
+        }
+    }
+}
